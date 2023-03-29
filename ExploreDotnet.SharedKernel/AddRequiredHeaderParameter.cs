9e94c69527df6e9ad2ba338ceefdbe75e46b9549
Agung Wicaksono <gungcaksono.id@gmail.com>
2023-03-29 21:52:32 +0800

    feat: add custom param header on swagger

---
+++ ExploreDotnet.SharedKernel/AddRequiredHeaderParameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ExploreDotnet.SharedKernel
+{
+    public class AddRequiredHeaderParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            (operation.Parameters ??= new List<OpenApiParameter>())
+                .Add(new OpenApiParameter
+                {
+                    Name = "X-Tenant-Id",
+                    In = ParameterLocation.Header,
+                    Required = false
+                });
+        }
+    }
+}
